d293372a485f21094fbd9f20a9a7cda64cb37eb1
Aryan <aryan16072001236809@gmail.com>
2022-09-09 16:17:52 +0530

    Setup Controllers and routes

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const port=8000;
 
 //use express router
 
-app.use('/',require('./routes'))
+app.use('/',require('./routes'));
 
-
-//setup the view engine
-
-app.use('view engine','ejs')
+app.set('view engine','ejs');
 app.set('views','./views')
 
 app.listen(port,function(err){
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for codeial</h1>')
+    
+    return res.render('home',{
+        title:"Home"
+    })
 }
 
 
