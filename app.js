da749f85ea6590b5e9eb6ef7cd3172a73323e0c2
Aryan <aryan16072001236809@gmail.com>
2022-09-16 04:07:54 +0530

    Adding scss and design some content

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware=require('node-sass-middleware');
+
+
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest: './assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+
+
+}))
 
 app.use(express.urlencoded());
 
