7c864304214560b9956913fdd353600d07622a4d
Aryan <aryan16072001236809@gmail.com>
2022-09-19 00:56:28 +0530

    Render the comments to the home by populate the user and post schema

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,7 +13,15 @@ module.exports.home = function(req, res){
 // }
 
 //populate the user for each post
-Post.find({}).populate('user').exec(function(err,posts){
+Post.find({})
+.populate('user')
+.populate({
+    path:'comments',
+    populate:{
+        path:'user'
+    }
+})
+.exec(function(err,posts){
     return res.render('home', {
         title: "Codeial||Home",
         posts:posts
