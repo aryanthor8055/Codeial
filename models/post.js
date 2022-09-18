94fc9f240d17394fd405f297e9a1ce0bf62f6a37
Aryan <aryan16072001236809@gmail.com>
2022-09-18 14:52:36 +0530

    check Authentication for posts and Create comment Schema

--- models/post.js
+++ models/post.js
@@ -10,7 +10,13 @@ const postSchema = new mongoose.Schema({
         type:  mongoose.Schema.Types.ObjectId,
         ref: 'User'
 
+    },
+    //include the array of ids of all comments in this post schema itself
+    comments:[{
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
     }
+]
 },{
     timestamps: true
 });
