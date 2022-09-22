cd190d92870e492eb84e24d134806b81135caab9
Aryan <aryan16072001236809@gmail.com>
2022-09-22 23:46:53 +0530

    Updating User profile and improve Scss of home

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
