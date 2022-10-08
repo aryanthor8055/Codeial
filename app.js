5a662dfe1b600e2bcdf84db9a8a8c35ff6de1fd6
Aryan <aryan16072001236809@gmail.com>
2022-10-08 23:58:47 +0530

    Adding Chat engine Using Socket.io

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
+
 
 app.use(sassMiddleware({
     src: './assets/scss',
